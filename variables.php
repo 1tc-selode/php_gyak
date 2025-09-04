e0831f0a0a8e57b3a7c8a22ce0ff81af55f67568
1tc-selode <oseletye@gmail.com>
2025-09-04 10:50:46 +0200

    variables

---
+++ variables.php
@@ -0,0 +1,18 @@
+<?php
+
+    /*
+        xampp apache mysql
+        git
+        github repo
+        vsc
+        <?php ... ?>
+        valtozok angolul
+    */ 
+
+    $name = "odett";
+    $age = 18;
+    $city = "kisuj";
+    echo var_dump($name);
+    echo "<br>";
+    echo "nev: $name eletkor: $age lakohely: $city";
+?>
