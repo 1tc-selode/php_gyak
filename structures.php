6ffba27853193f952665f1a111de9028faa870fd
1tc-selode <oseletye@gmail.com>
2025-09-08 09:23:45 +0200

    valami megint

--- structures.php
+++ structures.php
@@ -16,5 +16,55 @@
     }
     echo "<br>";
     $result = ($number % 2 == 0) ? "paros" : "paratlan";
-    echo "a(z) {$number} egy {$result} szam"
+    echo "a(z) {$number} egy {$result} szam";
+    echo "<br>";
+
+    //switch
+
+    //ciklus: for, while, foreach
+    //ciklussal irasd ki 1-10-ig a szamokat
+    for ($i=0; $i < 10; $i++) { 
+        $out = $i + 1;
+        echo "$out <br>";
+    }
+
+    //tombok
+    //hozz letre egy indexelt tombot 5 gyumolcsel es ird ki
+    $fruits = ["apple","banana","strawberry","raspberry","grape"];
+
+    for ($i=0; $i < count($fruits); $i++) { 
+        echo "$fruits[$i], ";
+    }
+    echo "<br>";
+
+    foreach($fruits as $fruit){
+        echo "$fruit, ";
+    }
+    echo "<br>";
+
+    //hozz letre usertombot ami tartalmazza a user nevet es az eletkorat
+    $users = [
+        "Kiss Pista" => 20,
+        "Kiss Éva" => 23,
+        "Kiss Géza" => 26,
+    ] ;
+
+    foreach ($users as $name => $age) {
+        echo"$name : $age <br>";
+    }
+    echo "<br>";
+
+    $students = [
+        ["name" => "Kovács Péter", "age" => 20],
+        ["name" => "Kovács Éva", "age" => 20],
+        ["name" => "Kovács Géza", "age" => 20]
+    ];
+
+    foreach ($students as $student) {
+        echo"$student[name] : $student[age]<br>";
+    }
+    echo "<br>";
+    foreach ($students as $student) {
+        echo"{$student['name']} : {$student['age']}<br>";
+    }
 ?>
